2845dc0ab0aef9892caea1da2d4b3e94da853dc9
thegr8manef <jedidimanef@gmail.com>
2023-03-15 15:03:51 +0100

    update worker and client

--- src/main/java/com/dev0mmj/depanini/entity/Client.java
+++ src/main/java/com/dev0mmj/depanini/entity/Client.java
@@ -4,8 +4,8 @@ import javax.persistence.Entity;
 
 @Entity
 public class Client  extends User{
-    public Client(Long id, String username,String password, String address, String image, Integer phone) {
-        super(id, username, password,address, image, phone);
+    public Client(Long id, String username,String password, String address_gov,String address_municipale, String image, Integer phone) {
+        super(id, username, password,address_gov,address_municipale, image, phone);
     }
 
     public Client() {
